4ea904485158a65872e79f06526d1ea8ad6fee32
Harish Ashok <68806913+Boldmoon@users.noreply.github.com>
2024-11-15 22:06:05 +0530

    Add files via upload

---
+++ threat.ps1
@@ -0,0 +1,60 @@
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+public class HiddenWindow {
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetConsoleWindow();
+    public static void Hide() {
+        IntPtr hWnd = GetConsoleWindow();
+        ShowWindow(hWnd, 0); // 0 = SW_HIDE
+    }
+}
+"@
+[HiddenWindow]::Hide()
+
+
+function Show-RandomMessage {
+    $messages = @(
+        "kys",
+        "You have been hacked",
+        "Cry about it",
+        "You are cooked",
+        "Performing self destruction",
+        "Please stanby, we are erasing your system",
+        "What ra?"
+    )
+    $randomMessage = $messages | Get-Random
+    [System.Windows.Forms.MessageBox]::Show($randomMessage, "International Threat", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+}
+
+function Open-RandomWebsite {
+    $websites = @(
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",  
+        "https://www.newyorker.com/news/daily-comment/monkey-see-monkey-click",
+        "https://www.youtube.com/watch?v=YbJOTdZBX1g",
+        "https://www.youtube.com/watch?v=HzL8lh39Y2Q",
+        "https://www.youtube.com/watch?v=RoKDsK8_ToQ",
+        "https://www.youtube.com/watch?v=gsLvizl5j4E"
+    )
+    $randomWebsite = $websites | Get-Random
+    Start-Process $randomWebsite
+}
+
+
+function Play-Sound {
+    [console]::beep(500, 300)  
+    Start-Sleep -Milliseconds 300
+}
+
+
+for ($i = 0; $i -lt 10; $i++) {
+    Start-Sleep -Seconds (Get-Random -Minimum 1 -Maximum 3)
+    Show-RandomMessage
+    Open-RandomWebsite
+    Play-Sound
+}
+
+
+[System.Windows.Forms.MessageBox]::Show("Location reported to CIA.", "You are done.", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
