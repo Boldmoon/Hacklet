80972160d1c8c2bd1f5c09f9a90a440a044225ae
Harish Ashok <68806913+Boldmoon@users.noreply.github.com>
2024-11-16 08:21:28 +0530

    Update cool.ps1

--- cool.ps1
+++ cool.ps1
@@ -1,88 +1,76 @@
-$errorActionPreference = 'SilentlyContinue'
+If (Get-Process -Name "powershell" -ErrorAction SilentlyContinue | Where-Object {$_.MainWindowTitle -like "Annoy-a-Friend*"} ) {
+    Exit
+}
 
-function Play-RandomSound {
-    $sounds = @(
-        [System.Windows.Forms.SystemSounds]::Beep,
-        [System.Windows.Forms.SystemSounds]::Asterisk,
-        [System.Windows.Forms.SystemSounds]::Exclamation,
-        [System.Windows.Forms.SystemSounds]::Hand,
-        [System.Windows.Forms.SystemSounds]::Question
+
+$Host.UI.RawUI.WindowTitle = "Critical System Alert"
+
+
+Function RandomBeep {
+    For ($i = 0; $i -lt 10; $i++) {
+        [console]::beep((Get-Random -Minimum 100 -Maximum 2000), (Get-Random -Minimum 100 -Maximum 500))
+        Start-Sleep -Milliseconds (Get-Random -Minimum 300 -Maximum 1000)
+    }
+}
+
+
+Function ToggleCapsLock {
+    Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class Keyboard {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+        public const int KEYEVENTF_EXTENDEDKEY = 0x1;
+        public const int KEYEVENTF_KEYUP = 0x2;
+    }
+"@
+    [Keyboard]::keybd_event(0x14, 0, 0, [UIntPtr]::Zero) # Key down
+    [Keyboard]::keybd_event(0x14, 0, 2, [UIntPtr]::Zero) # Key up
+}
+
+function Open-RandomWebsite {
+    $websites = @(
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",  
+        "https://www.youtube.com/watch?v=YbJOTdZBX1g",
+        "https://www.youtube.com/watch?v=HzL8lh39Y2Q",
+        "https://www.youtube.com/watch?v=RoKDsK8_ToQ",
+        "https://www.youtube.com/watch?v=gsLvizl5j4E"
     )
-    $randomSound = $sounds | Get-Random
-    $randomSound.Play()
+    $randomWebsite = $websites | Get-Random
+    Start-Process $randomWebsite
 }
 
-function Change-MouseCursor {
-    $cursorTypes = [System.Windows.Forms.Cursors].GetEnumValues() | Where-Object { $_ -ne [System.Windows.Forms.Cursors]::Default }
-    $randomCursor = $cursorTypes | Get-Random
-    [System.Windows.Forms.Cursor]::Current = $randomCursor
-}
-
-function Flash-Screen {
-    $flashColors = @("Red", "Blue", "Green", "Yellow", "Pink")
-    $randomColor = $flashColors | Get-Random
-    $flashScript = {
-        $flashForm = New-Object System.Windows.Forms.Form
-        $flashForm.WindowState = [System.Windows.Forms.FormWindowState]::Maximized
-        $flashForm.BackColor = [System.Drawing.Color]::$randomColor
-        $flashForm.TopMost = $true
-        $flashForm.Show()
-        Start-Sleep -Seconds 0.5
-        $flashForm.Hide()
-    }
-    Invoke-Command $flashScript
-}
-
-function Minimize-Maximize-Windows {
-    $visibleWindows = Get-Process | Where-Object { $_.MainWindowHandle -ne 0 }
-    foreach ($window in $visibleWindows) {
-        if ((Get-Random -Minimum 0 -Maximum 2) -eq 0) {
-            $window.MainWindowHandle | Out-Null
-            [System.Windows.Forms.SendKeys]::SendWait("{F9}")
-        } else {
-            $window.MainWindowHandle | Out-Null
-            [System.Windows.Forms.SendKeys]::SendWait("{F10}")
-        }
-    }
-}
-
-function Trigger-SystemError {
-    $errorMessages = @(
-        "SYSTEM_SERVICE_EXCEPTION",
-        "ATTEMPTED_EXECUTE_OF_NOEXECUTE_MEMORY",
-        "KMODE_EXCEPTION_NOT_HANDLED",
-        "SYSTEM_THREAD_EXCEPTION_NOT_HANDLED"
+Function FakeAlerts {
+    $messages = @(
+        "Warning: System Overheating!",
+        "Alert: Hard Disk Failure Detected!",
+        "Error 404: Computer Not Found",
+        "Your computer loves you ❤",
+        "Please standby, we are erasing your system!!",
+        "ALERT! SYSTEM COMPROMISED!"
     )
-    $randomError = $errorMessages | Get-Random
-    throw $randomError
+    $message = $messages | Get-Random
+    [System.Windows.Forms.MessageBox]::Show($message, "System Alert", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
 }
 
-function Exit-Madness {
-    $key = [System.Windows.Forms.Keys]::Control -bor [System.Windows.Forms.Keys]::Shift -bor [System.Windows.Forms.Keys]::K
-    $keyListener = New-Object System.Windows.Forms.Form
-    $keyListener.KeyPreview = $true
-    $keyListener.Add_KeyDown({
-        if ($_.Control -and $_.Shift -and $_.KeyCode -eq [System.Windows.Forms.Keys]::K) {
-            $keyListener.Close()
-            exit
-        }
-    })
-    $keyListener.ShowDialog()
+
+Function OpenRandomApps {
+    $apps = @("notepad", "calc", "mspaint")
+    $app = $apps | Get-Random
+    Start-Process $app
 }
 
-Start-Job { Exit-Madness }
 
-while ($true) {
-    Play-RandomSound
-    Change-MouseCursor
-    Flash-Screen
-    Minimize-Maximize-Windows
-
-    try {
-        Trigger-SystemError
-    } catch {
-
-    }
-
-    Start-Sleep -Seconds (Get-Random -Minimum 1 -Maximum 5)
+While ($true) {
+    RandomBeep
+    Start-Sleep -Seconds (Get-Random -Minimum 5 -Maximum 15)
+    ToggleCapsLock
+    Start-Sleep -Seconds (Get-Random -Minimum 10 -Maximum 20)
+    FakeAlerts
+    Start-Sleep -Seconds (Get-Random -Minimum 15 -Maximum 30)
+    OpenRandomApps
+    Start-Sleep -Seconds (Get-Random -Minimum 20 -Maximum 40)
+    Open-RandomWebsite
+    Start-Sleep -Seconds (Get-Random -Minimum 15 -Maximum 30)
 }
