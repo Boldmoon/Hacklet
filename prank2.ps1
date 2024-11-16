dee757d2ac69d8ea3bbd216f5bf2a62975c3e308
Harish Ashok <68806913+Boldmoon@users.noreply.github.com>
2024-11-16 08:43:00 +0530

    Update and rename prank1.ps1 to prank2.ps1

--- prank2.ps1
+++ prank2.ps1
@@ -1,4 +1,5 @@
 Add-Type -AssemblyName System.Windows.Forms
+
 Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
@@ -20,11 +21,14 @@ $videos = @(
 Function ConstantRandomMouseMove {
     $screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
     $screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
+
     While ($true) {
         If (CheckForStopShortcut) { Break }
+
         $x = Get-Random -Minimum 0 -Maximum $screenWidth
         $y = Get-Random -Minimum 0 -Maximum $screenHeight
         [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+
         Start-Sleep -Milliseconds (Get-Random -Minimum 50 -Maximum 150)
     }
 }
@@ -32,8 +36,9 @@ Function ConstantRandomMouseMove {
 Function PlayRandomVideos {
     While ($true) {
         If (CheckForStopShortcut) { Break }
+
         $video = $videos | Get-Random
-        Start-Process $video
+        Start-Process "chrome.exe" $video  # Specify the browser to open the video
         Start-Sleep -Seconds (Get-Random -Minimum 30 -Maximum 60)
     }
 }
@@ -47,8 +52,10 @@ Function ShowScaryAlerts {
         "CPU TEMPERATURE CRITICAL! 🔥",
         "FATAL ERROR: DEVICE SHUTDOWN IN PROGRESS."
     )
+
     While ($true) {
         If (CheckForStopShortcut) { Break }
+
         $message = $messages | Get-Random
         [System.Windows.Forms.MessageBox]::Show(
             $message, 
@@ -56,6 +63,7 @@ Function ShowScaryAlerts {
             [System.Windows.Forms.MessageBoxButtons]::OK, 
             [System.Windows.Forms.MessageBoxIcon]::Error
         )
+
         Start-Sleep -Seconds (Get-Random -Minimum 15 -Maximum 30)
     }
 }
@@ -67,17 +75,9 @@ Function CheckForStopShortcut {
     Return ($ctrl -and $shift -and $x)
 }
 
-$mouseJob = Start-Job -ScriptBlock { 
-    ConstantRandomMouseMove
-}
-
-$videoJob = Start-Job -ScriptBlock { 
-    PlayRandomVideos
-}
-
-$alertJob = Start-Job -ScriptBlock { 
-    ShowScaryAlerts
-}
+$mouseJob = Start-Job -ScriptBlock { ConstantRandomMouseMove }
+$videoJob = Start-Job -ScriptBlock { PlayRandomVideos }
+$alertJob = Start-Job -ScriptBlock { ShowScaryAlerts }
 
 While (-not (CheckForStopShortcut)) {
     Start-Sleep -Seconds 1
@@ -86,6 +86,7 @@ While (-not (CheckForStopShortcut)) {
 Stop-Job $mouseJob -Force
 Stop-Job $videoJob -Force
 Stop-Job $alertJob -Force
+
 [System.Windows.Forms.MessageBox]::Show(
     "Prank terminated. System restored to normal.", 
     "Prank Ended", 
