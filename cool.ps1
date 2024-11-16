845d9f465f37b6310d0c8d585007f05d693b1252
Harish Ashok <68806913+Boldmoon@users.noreply.github.com>
2024-11-16 08:27:34 +0530

    Update cool.ps1

--- cool.ps1
+++ cool.ps1
@@ -1,47 +1,44 @@
-If (Get-Process -Name "powershell" -ErrorAction SilentlyContinue | Where-Object {$_.MainWindowTitle -like "Annoy-a-Friend*"} ) {
-    Exit
+# Import .NET classes
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public static class KeyListener {
+    [DllImport("user32.dll")]
+    public static extern short GetAsyncKeyState(int vKey);
 }
-
-
-$Host.UI.RawUI.WindowTitle = "Critical System Alert"
-
-
-Function RandomBeep {
-    For ($i = 0; $i -lt 10; $i++) {
-        [console]::beep((Get-Random -Minimum 100 -Maximum 2000), (Get-Random -Minimum 100 -Maximum 500))
-        Start-Sleep -Milliseconds (Get-Random -Minimum 300 -Maximum 1000)
-    }
-}
-
-
-Function ToggleCapsLock {
-    Add-Type @"
-    using System;
-    using System.Runtime.InteropServices;
-    public class Keyboard {
-        [DllImport("user32.dll")]
-        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
-        public const int KEYEVENTF_EXTENDEDKEY = 0x1;
-        public const int KEYEVENTF_KEYUP = 0x2;
-    }
 "@
-    [Keyboard]::keybd_event(0x14, 0, 0, [UIntPtr]::Zero) # Key down
-    [Keyboard]::keybd_event(0x14, 0, 2, [UIntPtr]::Zero) # Key up
+
+
+$videos = @(
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "https://www.youtube.com/watch?v=YbJOTdZBX1g",
+    "https://www.youtube.com/watch?v=HzL8lh39Y2Q",
+    "https://www.youtube.com/watch?v=RoKDsK8_ToQ",
+    "https://www.youtube.com/watch?v=gsLvizl5j4E"
+)
+
+
+Function RandomMouseMove {
+    $screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
+    $screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
+    For ($i = 0; $i -lt 10; $i++) {
+        $x = Get-Random -Minimum 0 -Maximum $screenWidth
+        $y = Get-Random -Minimum 0 -Maximum $screenHeight
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+        Start-Sleep -Milliseconds (Get-Random -Minimum 100 -Maximum 500)
+    }
 }
 
-function Open-RandomWebsite {
-    $websites = @(
-        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",  
-        "https://www.youtube.com/watch?v=YbJOTdZBX1g",
-        "https://www.youtube.com/watch?v=HzL8lh39Y2Q",
-        "https://www.youtube.com/watch?v=RoKDsK8_ToQ",
-        "https://www.youtube.com/watch?v=gsLvizl5j4E"
-    )
-    $randomWebsite = $websites | Get-Random
-    Start-Process $randomWebsite
+
+Function OpenRandomVideos {
+    $video = $videos | Get-Random
+    Start-Process $video
 }
 
-Function FakeAlerts {
+
+Function ShowFunMessage {
     $messages = @(
         "Warning: System Overheating!",
         "Alert: Hard Disk Failure Detected!",
@@ -51,26 +48,26 @@ Function FakeAlerts {
         "ALERT! SYSTEM COMPROMISED!"
     )
     $message = $messages | Get-Random
-    [System.Windows.Forms.MessageBox]::Show($message, "System Alert", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
+    [System.Windows.Forms.MessageBox]::Show($message, "Fun Alert!")
 }
 
-
-Function OpenRandomApps {
-    $apps = @("notepad", "calc", "mspaint")
-    $app = $apps | Get-Random
-    Start-Process $app
+Function CheckForStopShortcut {
+    $ctrl = [KeyListener]::GetAsyncKeyState(0x11) -ne 0 # Ctrl key
+    $shift = [KeyListener]::GetAsyncKeyState(0x10) -ne 0 # Shift key
+    $x = [KeyListener]::GetAsyncKeyState(0x58) -ne 0 # 'X' key
+    Return ($ctrl -and $shift -and $x)
 }
 
 
 While ($true) {
-    RandomBeep
-    Start-Sleep -Seconds (Get-Random -Minimum 5 -Maximum 15)
-    ToggleCapsLock
+    If (CheckForStopShortcut) {
+        Break
+    }
+    RandomMouseMove
+    ShowFunMessage
+    OpenRandomVideos
     Start-Sleep -Seconds (Get-Random -Minimum 10 -Maximum 20)
-    FakeAlerts
-    Start-Sleep -Seconds (Get-Random -Minimum 15 -Maximum 30)
-    OpenRandomApps
-    Start-Sleep -Seconds (Get-Random -Minimum 20 -Maximum 40)
-    Open-RandomWebsite
-    Start-Sleep -Seconds (Get-Random -Minimum 15 -Maximum 30)
 }
+
+
+[System.Windows.Forms.MessageBox]::Show("Location reported to CIA.", "You are done.", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
